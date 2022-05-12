72386320522f25e8f8ccd825a79c4e7aa14bd5c3
Alexandre Costa <alexcosta1889@gmail.com>
2022-05-12 16:35:49 +0100

    burgermenu working

--- dream-green/src/App.js
+++ dream-green/src/App.js
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-dom";
-import Footer from "./Components/Footer";
-
+import React from "react";
+import Footer from "./Components/Footer/Footer";
+import BurgerMenu from "./Components/BurgerMenu/BurgerMenu";
+import { Routes, Route } from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <BurgerMenu />
       <Routes>
         <Route path="/footer" element={<Footer />}></Route>
       </Routes>
---
+++ dream-green/src/Components/BurgerMenu/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './Sidebar.css';
+
+export default props => {
+    return (
+        <Menu>
+            <a className="menu-item" href="/">
+                About
+            </a>
+            <a className="menu-item" href="/services">
+                Services
+            </a>
+            <a className="menu-item" href="/contacts">
+                Contacts
+            </a>
+            <a className="menu-item" href="/desserts">
+                Desserts
+            </a>
+        </Menu>
+    );
+};
