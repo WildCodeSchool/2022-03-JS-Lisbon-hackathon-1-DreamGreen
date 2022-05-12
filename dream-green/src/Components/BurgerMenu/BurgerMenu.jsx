8889b55ecfcb1946fe1b9e4363de7f317d540b27
Alexandre Costa <alexcosta1889@gmail.com>
2022-05-12 23:55:46 +0100

    burgewr menu logo

--- dream-green/src/Components/BurgerMenu/BurgerMenu.jsx
+++ dream-green/src/Components/BurgerMenu/BurgerMenu.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './BurgerMenu.css';
 import Sidebar from './Sidebar';
-
+import Logo from '../Images/Bananas.png';
 
 function BurgerMenu() {
     return (
-        <div className="App" id="outer-container">
-            <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-            <div id="page-wrap">
-
+        <div className='logo'>
+            <div className="App" id="outer-container">
+                <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+                <div id="page-wrap">
+                </div>
+            </div>
+            <div className='logo-img'>
+                <img src={Logo}></img>
             </div>
         </div>
     );
