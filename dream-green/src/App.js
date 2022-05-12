a42317ddc0e2b8f1df9a4b4a02dec3eefd706aa6
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-05-12 17:45:50 +0100

    added footer component

--- dream-green/src/App.js
+++ dream-green/src/App.js
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-dom";
-import Footer from "./Components/Footer";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import Footer from "./Components/Footer/Footer.jsx";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Footer />
       <Routes>
         <Route path="/footer" element={<Footer />}></Route>
       </Routes>
