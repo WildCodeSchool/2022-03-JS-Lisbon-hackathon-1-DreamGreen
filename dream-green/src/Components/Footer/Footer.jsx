576c77020fe76c9883f3bf0a4a526ac09366fcba
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-05-12 19:29:57 +0100

    added desktop and mobile css

--- dream-green/src/Components/Footer/Footer.jsx
+++ dream-green/src/Components/Footer/Footer.jsx
@@ -12,52 +12,76 @@ import "./Footer.css";
 function Footer(props) {
   return (
     <div className="footerContainer">
-      <div className="contact-container">
-        <div className="contact-icon">
-          <h3 className="icon-description">
-            <icon className="icon-contact">
-              <AiTwotonePhone />
-            </icon>
-            Contact
-          </h3>
-          <p>+351214443322</p>
+      <div className="blank"></div>
+      <div className="footer-contact-container">
+        <div className="contact-container">
+          <div className="contact-card1">
+            <div className="contact-icon">
+              <h3 className="icon-description">
+                <icon className="icon-contact">
+                  <AiTwotonePhone />
+                </icon>
+                Contact
+              </h3>
+              <p>+351214443322</p>
+            </div>
+            <div className="contact-icon1">
+              <h3 className="icon-description">
+                <icon className="icon-contact">
+                  <AiOutlineMail />
+                </icon>
+                Email
+              </h3>
+              <p>dream-green@wildcode.com</p>
+            </div>
+          </div>
+
+          <div className="contact-card2">
+            <div className="contact-icon">
+              <h3 className="icon-description">
+                <icon className="icon-contact">
+                  <RiMapPinFill />
+                </icon>
+                Office
+              </h3>
+              <p>Rua Visconde Santarém, 73B, Lisbon</p>
+            </div>
+            <div className="contact-icon">
+              <h3 className="icon-description">
+                <icon className="icon-contact">
+                  <FiMap />
+                </icon>
+                Stockroom
+              </h3>
+              <p>Rua Conde Redondo, 84, Lisbon</p>
+            </div>
+          </div>
         </div>
-        <div className="contact-icon">
-          <h3 className="icon-description">
-            <icon className="icon-contact">
-              <AiOutlineMail />
-            </icon>
-            Email
-          </h3>
-          <p>dream-green@wildcode.com</p>
-        </div>
-        <div className="contact-icon">
-          <h3 className="icon-description">
-            <icon className="icon-contact">
-              <RiMapPinFill />
-            </icon>
-            Office
-          </h3>
-          <p>Rua Visconde Santarém, 73B, Lisbon</p>
-        </div>
-        <div className="contact-icon">
-          <h3 className="icon-description">
-            <icon className="icon-contact">
-              <FiMap />
-            </icon>
-            Stockroom
-          </h3>
-          <p>Rua Conde Redondo, 84, Lisbon</p>
-        </div>
-      </div>
-      <div className="social-container">
-        <h3 className="social-title">The Team behind the Project</h3>
-        <div className="QR-code_container">
-          <img className="qr-code" src={Patricia} alt="Patricia QR code"></img>
-          <img className="qr-code" src={Alexandre} alt="Patricia QR code"></img>
-          <img className="qr-code" src={Joao} alt="Patricia QR code"></img>
-          <img className="qr-code" src={Lourenço} alt="Patricia QR code"></img>
-          <img className="qr-code" src={Santiago} alt="Patricia QR code"></img>
+        <div className="social-container">
+          <h3 className="social-title">The Team behind the Project</h3>
+          <div className="QR-code_container">
+            <img
+              className="qr-code"
+              src={Patricia}
+              alt="Patricia QR code"
+            ></img>
+            <img
+              className="qr-code"
+              src={Alexandre}
+              alt="Patricia QR code"
+            ></img>
+            <img className="qr-code" src={Joao} alt="Patricia QR code"></img>
+            <img
+              className="qr-code"
+              src={Lourenço}
+              alt="Patricia QR code"
+            ></img>
+            <img
+              className="qr-code"
+              src={Santiago}
+              alt="Patricia QR code"
+            ></img>
+          </div>
         </div>
       </div>
     </div>
