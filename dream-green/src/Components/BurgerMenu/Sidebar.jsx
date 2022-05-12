5594790a294fc578091b0b19d7c43d7a3c1194ed
Alexandre Costa <alexcosta1889@gmail.com>
2022-05-12 21:49:29 +0100

    added footer route

--- dream-green/src/Components/BurgerMenu/Sidebar.jsx
+++ dream-green/src/Components/BurgerMenu/Sidebar.jsx
@@ -3,19 +3,22 @@ import { slide as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return (
-    <Menu>
-      <a className="menu-item" href="/">
-        About
-      </a>
-      <a className="menu-item" href="/services">
-        Services
-      </a>
-      <a className="menu-item" href="/contacts">
-        Contacts
-      </a>
-    </Menu>
-  );
+    return (
+        <Menu>
+            <a className="menu-item" href="/">
+                About
+            </a>
+            <a className="menu-item" href="/services">
+                Services
+            </a>
+            <a className="menu-item" href="/contacts">
+                Contacts
+            </a>
+            <a className="menu-item" href="/footer">
+                Footer
+            </a>
+        </Menu>
+    );
 }
 
 export default Sidebar;
