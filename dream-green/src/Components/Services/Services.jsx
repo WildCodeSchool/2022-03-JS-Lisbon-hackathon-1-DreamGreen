ee5b7983d203500b4439a34fd6cbb3ea3fb5c1e8
Sa7i <joao-sassetti_student2022@wilder.school>
2022-05-12 23:55:49 +0100

    change images for slideshow

--- dream-green/src/Components/Services/Services.jsx
+++ dream-green/src/Components/Services/Services.jsx
@@ -5,10 +5,12 @@ import { Pagination, Navigation } from "swiper";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import SlideThree from '../Images/pexels-pixabay-33783.jpg';
+import SlideOne from '../Images/pexels-pixabay-33783.jpg';
 import SlideTwo from '../Images/pexels-jannis-knorr-2933243.jpg';
-import SlideOne from '../Images/pexels-magda-ehlers-2569839.jpg';
-
+import SlideThree from '../Images/pexels-magda-ehlers-2569839.jpg';
+import SlideFour from '../Images/pexels-aleksejs-bergmanis-681335.jpg'
+import SlideFive from '../Images/pexels-pixabay-209251.jpg'
+import SlideSix from '../Images/pexels-tima-miroshnichenko-6169056.jpg'
 
 function Services() {
     return (
@@ -17,9 +19,9 @@ function Services() {
                 <h1>Our Services</h1>
             </div>
             <Swiper pagination={true} modules={[Pagination, Navigation]} navigation={true} loop={true} className="mySwiper">
-                <SwiperSlide><img src={SlideOne} alt='' /></SwiperSlide>
-                <SwiperSlide><img src={SlideTwo} alt='' /></SwiperSlide>
-                <SwiperSlide><img src={SlideThree} alt='' /></SwiperSlide>
+                <SwiperSlide><img src={SlideFive} alt='' /></SwiperSlide>
+                <SwiperSlide><img src={SlideFour} alt='' /></SwiperSlide>
+                <SwiperSlide><img src={SlideSix} alt='' /></SwiperSlide>
             </Swiper>
             <div className='services-description'>
                 <p>
