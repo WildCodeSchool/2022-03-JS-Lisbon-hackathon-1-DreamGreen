ca7ee926cfe8d4067f83fc87b6a6992163ce2c95
santilieb <santiago.liebrecht@gmail.com>
2022-05-12 17:16:48 +0100

    Added all fields to form

--- dream-green/src/Components/Form/Form.jsx
+++ dream-green/src/Components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import "./Form.css"
+import "./bootstrap.css"
 
 function Form() {
     const { register, handleSubmit, formState: { errors } } = useForm();
@@ -8,22 +9,56 @@ function Form() {
     console.log(errors);
 
     return (
-        <form className="form" onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Company Name" {...register("Company Name", { required: true })} />
-            <input type="email" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
-            <input type="tel" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
-            <select {...register("Oil quantity", { required: true })}>
-                <option value="10L-20L">10L-20L</option>
-                <option value="20L-30L">20L-30L</option>
-                <option value="30L-40L">30L-40L</option>
-                <option value="40L-50L">40L-50L</option>
-                <option value="+50L">+50L</option>
-            </select>
-            <input type="datetime-local" placeholder="Collection date" {...register("Collection date", { required: true })} />
-            <input type="text" placeholder="Additional comments" {...register("Additional comments", {})} />
+        <div>
+            <div>
 
-            <input type="submit" />
-        </form>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Company Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" placeholder="Company Name" {...register("Company Name", { required: true })} />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Mobile number</label>
+                    <div class="col-sm-10">
+                        <input type="tel" class="form-control" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Oil quantity</label>
+                    <div class="col-sm-10">
+                        <select class="form-control" {...register("Oil quantity", { required: true })}>
+                            <option value="10L-20L">10L-20L</option>
+                            <option value="20L-30L">20L-30L</option>
+                            <option value="30L-40L">30L-40L</option>
+                            <option value="40L-50L">40L-50L</option>
+                            <option value="+50L">+50L</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Collection date</label>
+                    <div class="col-sm-10">
+                        <input type="datetime-local" class="form-control" placeholder="Collection date" {...register("Collection date", { required: true })} />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Additional info</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" placeholder="Additional comments" {...register("Additional comments", {})} />
+                    </div>
+                </div>
+                <input className='btn btn-primary' type="submit" />
+            </form>
+        </div>
     );
 }
 
