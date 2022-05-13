664992a53f9ff7b28f004e07c6b63c47d183e17f
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-05-13 11:16:43 +0100

    fix error

--- dream-green/src/Components/Footer/Footer.jsx
+++ dream-green/src/Components/Footer/Footer.jsx
@@ -94,22 +94,23 @@ function Footer() {
               </icon>
             </div>
 
-          <div className="QR-code_container">
-            <icon className="Linkedin-code">
-              <SiLinkedin />
-            </icon>
-            <icon className="Linkedin-code">
-              <SiLinkedin />
-            </icon>
-            <icon className="Linkedin-code">
-              <SiLinkedin />
-            </icon>
-            <icon className="Linkedin-code">
-              <SiLinkedin />
-            </icon>
-            <icon className="Linkedin-code">
-              <SiLinkedin />
-            </icon>
+            <div className="QR-code_container">
+              <icon className="Linkedin-code">
+                <SiLinkedin />
+              </icon>
+              <icon className="Linkedin-code">
+                <SiLinkedin />
+              </icon>
+              <icon className="Linkedin-code">
+                <SiLinkedin />
+              </icon>
+              <icon className="Linkedin-code">
+                <SiLinkedin />
+              </icon>
+              <icon className="Linkedin-code">
+                <SiLinkedin />
+              </icon>
+            </div>
           </div>
         </div>
       </div>
