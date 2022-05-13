eae6bb05a35fb6aae66e2af0c9481225630d4743
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-05-13 12:00:58 +0100

    added menu code to burgerMenu

--- dream-green/src/Components/BurgerMenu/Sidebar.jsx
+++ dream-green/src/Components/BurgerMenu/Sidebar.jsx
@@ -2,31 +2,25 @@ import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 
-
-
 function Sidebar() {
-    return (
-
-        <div>
-            <Menu>
-                <a className="menu-item" href="/">
-                    About
-                </a>
-                <a className="menu-item" href="/services">
-                    Services
-                </a>
-                <a className="menu-item" href="/contacts">
-                    Contacts
-                </a>
-                <a className="menu-item" href="/footer">
-                    Footer
-                </a>
-            </Menu>
-        </div>
-
-
-
-    );
+  return (
+    <div>
+      <Menu>
+        <a className="menu-item" href="#about">
+          About
+        </a>
+        <a className="menu-item" href="#schedule">
+          Schedule Collection
+        </a>
+        <a className="menu-item" href="#services">
+          Services
+        </a>
+        <a className="menu-item" href="#footer ">
+          Contacts
+        </a>
+      </Menu>
+    </div>
+  );
 }
 
 export default Sidebar;
